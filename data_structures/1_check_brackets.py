d5025bd32627d49a5ef7b22ff7a918b4389dcf20
Alex Ramsdell <alexramsdell@gmail.com>
2016-09-30 19:41:26 -0700

    passed check_brackets.py

--- data_structures/1_check_brackets.py
+++ data_structures/1_check_brackets.py
@@ -1,16 +1,37 @@
+#python2 
+
 '''
+Status: 'Good job! (Max time used: 0.08/1.00, max memory used: 9519104/536870912.)' 
+
 Alex Ramsdell
 Coursera Data Structures Week 1 Programming Assignment
 
+Check Brackets in the Code
+
 Constraints: the length of S is at least 1 and at most 10^5.
-Input:  one string 𝑆 which consists of big and small latin letters, digits, punctuation marks and brackets from the set []{}().
+Input:  one string S which consists of big and small latin letters, digits, punctuation marks and brackets from the set []{}().
 Output: Output 'Success' if the code uses brackets correctly and the 1-based index of the first unmatched closing bracket otherwise. 
 
+Cases: Empty String, Missing closing, missing opening, incorrect closing.
+
+empty: '' -- stack empty
+missing closing bracket: [[] -- stack not empty at end of code
+missing opening bracket: ]]) -- stack underflow 
+incorrect closing: [} -- stack pop mismatch
+
 '''
-code_string = raw_input()
+
+def top(stack):
+    return None if empty(stack) else stack[-1][0] 
+
+def empty(stack):
+    return not len(stack)
 
 def stack(code):
+
     stack = []
+    is_valid = True
+    error_index = -1
     opening = [
         '(',
         '[',
@@ -22,25 +43,37 @@ def stack(code):
         '}': '{',
     }
 
-    for c in code:
+    for index, char in enumerate(code):
 
-        if c in opening:
-            stack.append(c)
+        if char in opening:
+            stack.append((char, index))
 
-        elif c in closing:
+        elif char in closing:
 
-            if not len(stack): 
+            # nothing to pop (closing bracket before opening)
+            if empty(stack):
+                error_index = index
+                is_valid = False
+                break
 
-                print 'invalid'
-                return;
+            # mismatched closing bracket
+            # give current index in both cases 
+            elif top(stack) != closing[char]:
+                error_index = index
+                is_valid = False
+                break
 
-            if closing[c] == stack[-1]:
+            # its a match
+            elif top(stack) == closing[char]:
                 stack.pop()
-            else:
-                print 'invalid'
-                return
 
-    print 'valid' if not len(stack) else 'invalid'
 
-code = raw_input('enter your code for me to balance:').strip()
-stack(code)
+    if is_valid and not empty(stack):
+        is_valid = False
+        char, error_index = stack.pop()
+
+    return is_valid, error_index + 1
+
+code_string = raw_input()
+is_valid, error_index = stack(code_string)
+print 'Success' if is_valid else error_index  
