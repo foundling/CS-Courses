a0b0a972da0bc52d7917db514fb1b52b6fb4d441
Alex Ramsdell <alexramsdell@gmail.com>
2016-10-19 17:59:46 -0700

    first go at pattern matching, recording indexes

---
+++ general_practice/pattern_matching.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+'''
+
+    Find all instances of pattern in text t. 
+    Return index of match for each distinct match, or -1 if none.
+
+'''
+
+def find_match_indexes(pattern, text):
+
+    match_locations = []
+    pattern_index = 0
+
+    for text_index, char in enumerate(text):
+
+        # end of pattern
+        if pattern_index >= len(pattern):
+            match_locations.append(text_index - len(pattern))  
+            pattern_index = 0
+
+        # another matched text character from pattern 
+        if char == pattern[pattern_index]:
+            pattern_index += 1
+
+        # not a match. continue but make sure pattern_index is back at 0
+        else:
+            pattern_index = 0
+
+    return match_locations 
+
+
+pattern = ' '
+text = 'Every good boy does fine Every day'
+
+print find_match_indexes(pattern, text)
+# should return 0 and 25 
