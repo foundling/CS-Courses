2c78a1ff6a59a8d450831d17f3dace2460683945
Alex Ramsdell <alexramsdell@gmail.com>
2016-10-21 12:50:29 -0700

    rework pattern_matching

--- general_practice/pattern_matching.py
+++ general_practice/pattern_matching.py
@@ -11,27 +11,22 @@
 def find_match_indexes(pattern, text):
 
     match_locations = []
-    pattern_index = 0
-    pattern_length = len(pattern)
+    pattern_index, pattern_length = 0, len(pattern)
 
     for text_index, char in enumerate(text):
 
-        # check for end of pattern
-        if pattern_index >= pattern_length:
-            match_locations.append(text_index - pattern_length)  
-            pattern_index = 0
-
-        # also, check for another matched text character from pattern 
-        if char == pattern[pattern_index]:
+        # check for single character match -- should go before checking for full pattern match
+        if pattern_index < pattern_length and char == pattern[pattern_index]:
             pattern_index += 1
 
-        # or it's not a match. so reset pattern_index 
-        else:
+        # check for complete match
+        elif pattern_index == pattern_length - 1:
+            match_locations.append(text_index - pattern_index)
             pattern_index = 0
 
-    # check if pattern matches end of text
-    if pattern_index == pattern_length:
-        print text_index, pattern_length
-        match_locations.append(text_index - (pattern_length - 1))  
+        # no match, reset accrued pattern index
+        else:
+            pattern_index = 0
+            
 
     return match_locations
